e908132ccdb711124048eb41fa7f4002b3db05e3
oliviesalat <semyon.suslov@gmail.com>
2025-02-06 14:27:32 +0100

    add Breeze api

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,9 @@
 <?php
 
-use App\Http\Controllers\StaticPageController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return 'hello';
-});
-Route::get('/home', function () {
-    return "welcome";
+    return ['Laravel' => app()->version()];
 });
 
-Route::get('/index', [StaticPageController::class, 'index'])->name('index');
-
-Route::get('/shop', function () {return view('shop');})->name('shop');
+require __DIR__.'/auth.php';
