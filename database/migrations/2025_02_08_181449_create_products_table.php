b0c496f73226bd4136b7b10c398dc2fbf0d594c2
oliviesalat <semyon.suslov@gmail.com>
2025-02-08 19:17:33 +0100

    add product model -m

--- database/migrations/2025_02_08_181449_create_products_table.php
+++ database/migrations/2025_02_08_181449_create_products_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('description');
+            $table->integer('price');
+            $table->softDeletes();
             $table->timestamps();
         });
     }
